7d7b3b8949e7e0cdcaa86aad4f35da8b5e66574a
ion <ion2005@mail.ru>
2025-06-06 11:37:45 +0300

    Update WebHookService

--- semestr0v04ka3/impl/src/main/java/com/technokratos/service/general/impl/WebHookServiceImpl.java
+++ semestr0v04ka3/impl/src/main/java/com/technokratos/service/general/impl/WebHookServiceImpl.java
@@ -3,6 +3,7 @@ package com.technokratos.service.general.impl;
 import com.technokratos.service.general.WebHookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -19,15 +20,17 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class WebHookServiceImpl implements WebHookService {
+
+    @Value("${web-hook.get-solution}")
+    private String GET_SOLUTION_URL_PATTERN;
+
     @Async
     @Override
     public void notifyExternalService(String url, String secret, UUID solutionId) {
         RestTemplate restTemplate = new RestTemplate();
         Map<String, Object> payload = new HashMap<>();
-        String URL_PATTERN = "http://localhost:8080/api/v1/solutions/%s";
 
-
-        payload.put("link", URL_PATTERN.formatted(solutionId));
+        payload.put("link", GET_SOLUTION_URL_PATTERN.formatted(solutionId));
 
         HttpHeaders headers = new HttpHeaders();
 
